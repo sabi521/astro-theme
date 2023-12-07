dc0a26b1bce270f9ab7c617e949905007cb3a206
sarbjeet <sabi521@gmail.com>
2023-12-07 12:57:57 -0500

    new updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  experimental: { assets: true },
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
