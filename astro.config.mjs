602dee33022c8fed7685fc406495ff480cd085e6
sarbjeet <sabi521@gmail.com>
2023-12-07 17:09:17 -0500

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  integrations: [tailwind()],
+  experimental: { assets: true },
 });
