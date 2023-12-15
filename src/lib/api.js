57d1fbe6d67feef0d6d1318f7fee611f6442d963
sarbjeet <sabi521@gmail.com>
2023-12-15 15:43:42 -0500

    single guide page updated

--- src/lib/api.js
+++ src/lib/api.js
@@ -85,6 +85,7 @@ export async function GuideSidebar(){
                     edges {
                       node {
                         title
+                        uri
                       }
                     }
                   }
