71d93017fa26d5e90ff75102319c7fa767df0473
sarbjeet <sabi521@gmail.com>
2024-01-30 14:15:32 -0500

    prefetch testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
 import netlify from "@astrojs/netlify/functions";
-
 import sitemap from "@astrojs/sitemap";
 
 //https://docs.astro.build/en/guides/integrations-guide/sitemap/
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://preeminent-mochi-ac18b5.netlify.app/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), prefetch({
+    // Allow up to three links to be prefetched concurrently
+    throttle: 3,
+  }),],
   output: "server",
   adapter: netlify()
 });
