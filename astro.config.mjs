0b922792ff873de57e43e9c612ea52946ccc99eb
sarbjeet <sabi521@gmail.com>
2024-01-30 10:47:08 -0500

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
-
 import netlify from "@astrojs/netlify/functions";
 
+import sitemap from "@astrojs/sitemap";
+
+//https://docs.astro.build/en/guides/integrations-guide/sitemap/
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://preeminent-mochi-ac18b5.netlify.app/',
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
