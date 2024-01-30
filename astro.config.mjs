fe448273a21e569ca427a546fa081fe9bd1aedb2
sarbjeet <sabi521@gmail.com>
2024-01-30 14:52:13 -0500

    script type module removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,12 @@ import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 
 //https://docs.astro.build/en/guides/integrations-guide/sitemap/
-import prefetch from "@astrojs/prefetch";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://preeminent-mochi-ac18b5.netlify.app/',
-  integrations: [tailwind(), sitemap(), prefetch({
-    // Allow up to three links to be prefetched concurrently
-    throttle: 3,
-  }),],
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
