6634b957bc149581debe0ade1e7ca0e5684144d0
sarbjeet <sabi521@gmail.com>
2024-02-01 16:06:51 -0500

    sitemap removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
 import netlify from "@astrojs/netlify/functions";
-import sitemap from "@astrojs/sitemap";
+
 
 //https://docs.astro.build/en/guides/integrations-guide/sitemap/
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://preeminent-mochi-ac18b5.netlify.app/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: "server",
   adapter: netlify()
 });
